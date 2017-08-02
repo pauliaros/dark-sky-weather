a640b6937c9456b1513f08852fd2a38cd5f6fb51
Paulina Aros <aros.pau@gmail.com>
2017-08-01 23:09:15 -0400

    ahi nomas

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$("#pred").click(function(e){
+            e.preventDefault();
+            window.location = "index1.html";
+        });
+
+$("#back").click(function(e){
+            e.preventDefault();
+            window.location = "index.html";
+        });
